e5609532ff5fd32fb6b5c0e80a3d3d80340fec54
Trevor <ifndefdeadmau5@gmail.com>
2017-03-07 17:18:37 +0900

    Update ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-find-dom-node': 'off',
+    'react/prefer-stateless-function': 'off',
+    'class-methods-use-this': 'off',
+  },
 };
