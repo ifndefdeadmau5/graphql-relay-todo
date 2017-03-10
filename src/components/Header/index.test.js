4f26346f6c132baeb5eb5ba577d985194d9b96b0
Trevor <ifndefdeadmau5@gmail.com>
2017-03-10 20:45:57 +0900

    Add first snapshot test case
    1.Created ‘Header’ component snapshot.
    2.To use both jest and webpack2, convert ES modules to CommonJS as jest runs on node environment.

---
+++ src/components/Header/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from './index';
+
+test('Link changes the class when hovered', () => {
+  const component = renderer.create(
+    <Header />,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
